1d786f70bd5974a4e99b0bcf14192aa97c997ee4
Agilan <agilanbala@gmail.com>
2025-03-24 16:19:45 +0530

    my clinics 7

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true})) 
 app.use(cors({
-    origin: true, // Reflects the request origin dynamically
+    origin: process.env.FRONTEND_URL, // Reflects the request origin dynamically
     methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true, // Allows cookies/auth headers
